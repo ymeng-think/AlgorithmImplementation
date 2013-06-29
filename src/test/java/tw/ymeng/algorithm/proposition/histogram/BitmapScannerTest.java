110f5ade4f7c84bb9839119a06afd481e75c7b43
Meng Yu <ymeng@thoughtworks.com>
2013-06-29 13:29:22 +0800

    [ymeng] histogram: add more test cases

--- src/test/java/tw/ymeng/algorithm/proposition/histogram/BitmapScannerTest.java
+++ src/test/java/tw/ymeng/algorithm/proposition/histogram/BitmapScannerTest.java
@@ -4,6 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import static tw.ymeng.algorithm.proposition.histogram.BitmapBuilder.*;
 
@@ -76,6 +77,34 @@ public class BitmapScannerTest {
     }
 
     @Test
+    public void should_get_max_area_when_rectangles_are_arranged_as_reversed_ladder() {
+        BitmapScanner bitmapScanner = new BitmapScanner(build(3, new boolean[]{
+                O, O, O,
+                X, O, O,
+                X, X, O
+        }));
+
+        Rectangle rectangle = bitmapScanner.getMaxRectangle();
+
+        assertThat(rectangle.width(), is(2));
+        assertThat(rectangle.height(), is(2));
+    }
+
+    @Test
+    public void should_get_max_area_when_rectangles_are_arranged_as_mirrored_and_reversed_ladder() {
+        BitmapScanner bitmapScanner = new BitmapScanner(build(3, new boolean[]{
+                O, O, O,
+                O, O, X,
+                O, X, X
+        }));
+
+        Rectangle rectangle = bitmapScanner.getMaxRectangle();
+
+        assertThat(rectangle.width(), is(2));
+        assertThat(rectangle.height(), is(2));
+    }
+
+    @Test
     public void should_get_max_area_when_rectangles_are_arranged_as_pyramid() {
         BitmapScanner bitmapScanner = new BitmapScanner(build(5, new boolean[]{
                 X, X, O, X, X,
@@ -87,5 +116,46 @@ public class BitmapScannerTest {
 
         assertThat(rectangle.width(), is(3));
         assertThat(rectangle.height(), is(2));
+    }
+
+    @Test
+    public void should_get_max_area_when_rectangles_are_arranged_as_reversed_pyramid() {
+        BitmapScanner bitmapScanner = new BitmapScanner(build(5, new boolean[]{
+                O, O, O, O, O,
+                X, O, O, O, X,
+                X, X, O, X, X,
+        }));
+
+        Rectangle rectangle = bitmapScanner.getMaxRectangle();
+
+        assertThat(rectangle.width(), is(3));
+        assertThat(rectangle.height(), is(2));
+    }
+
+    @Test
+    public void should_get_max_area_when_rectangles_are_arranged_as_wall() {
+        BitmapScanner bitmapScanner = new BitmapScanner(build(3, new boolean[]{
+                O, O, O,
+                O, O, O,
+                O, O, O,
+        }));
+
+        Rectangle rectangle = bitmapScanner.getMaxRectangle();
+
+        assertThat(rectangle.width(), is(3));
+        assertThat(rectangle.height(), is(3));
+    }
+
+    @Test
+    public void should_get_max_area_when_there_are_no_rectangles() {
+        BitmapScanner bitmapScanner = new BitmapScanner(build(3, new boolean[]{
+                X, X, X,
+                X, X, X,
+                X, X, X,
+        }));
+
+        Rectangle rectangle = bitmapScanner.getMaxRectangle();
+
+        assertThat(rectangle, sameInstance(Rectangle.NULL));
     }
 }
