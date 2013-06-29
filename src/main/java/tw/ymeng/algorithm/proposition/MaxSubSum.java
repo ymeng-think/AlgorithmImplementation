c4d4afbc05d3eb9dbc3c232fab674c45a4d5b80d
Meng Yu <ymeng@thoughtworks.com>
2013-06-29 14:45:46 +0800

    [ymeng] Improve alogrithm of maximum contiguous subsequence sum

--- src/main/java/tw/ymeng/algorithm/proposition/MaxSubSum.java
+++ src/main/java/tw/ymeng/algorithm/proposition/MaxSubSum.java
@@ -1,49 +1,91 @@
 package tw.ymeng.algorithm.proposition;
 
-import static java.lang.Math.max;
-
 public final class MaxSubSum {
 
-    public static int maxSubSum(int[] array) {
-        return maxSumRec(array, 0, array.length - 1);
+    public static final MaxSubSum DivideAndConquerAlgorithm = new MaxSubSum(new DivideAndConquer());
+    public static final MaxSubSum LinearTimeAlgorithm = new MaxSubSum(new LinearTimeAlgorithm());
+
+    private MaximumContiguousSubSequenceSum algorithm;
+
+    private MaxSubSum(MaximumContiguousSubSequenceSum algorithm) {
+        this.algorithm = algorithm;
     }
 
-    private static int maxSumRec(int[] array, int left, int right) {
-        if (left == right) {
-            if (array[left] > 0) {
-                return array[left];
-            } else {
-                return 0;
-            }
-        }
-
-        int center = (left + right) / 2;
-        int maxLeftSum = maxSumRec(array, left, center);
-        int maxRightSum = maxSumRec(array, center + 1, right);
-
-        int maxLeftBorderSum = 0, leftBorderSum = 0;
-        for (int i = center; i >= left; i--) {
-            leftBorderSum += array[i];
-            if (leftBorderSum > maxLeftBorderSum) {
-                maxLeftBorderSum = leftBorderSum;
-            }
-        }
-
-        int maxRightBorderSum = 0, rightBorderSum = 0;
-        for (int i = center + 1; i <= right; i++) {
-            rightBorderSum += array[i];
-            if (rightBorderSum > maxRightBorderSum) {
-                maxRightBorderSum = rightBorderSum;
-            }
-        }
-
-        return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
+    public int maxSubSum(int[] array) {
+        return algorithm.max(array);
     }
 
-    private static int max3(int a, int b, int c) {
-        return max(max(a, b), c);
+
+
+    private static interface MaximumContiguousSubSequenceSum {
+        int max(int[] array);
     }
 
-    private MaxSubSum() {
+    private static class DivideAndConquer implements MaximumContiguousSubSequenceSum {
+
+        @Override
+        public int max(int[] array) {
+            return maxSumRec(array, 0, array.length - 1);
+        }
+
+        private int maxSumRec(int[] array, int left, int right) {
+            if (left == right) {
+                if (array[left] > 0) {
+                    return array[left];
+                } else {
+                    return 0;
+                }
+            }
+
+            int center = (left + right) / 2;
+            int maxLeftSum = maxSumRec(array, left, center);
+            int maxRightSum = maxSumRec(array, center + 1, right);
+
+            int maxLeftBorderSum = 0, leftBorderSum = 0;
+            for (int i = center; i >= left; i--) {
+                leftBorderSum += array[i];
+                if (leftBorderSum > maxLeftBorderSum) {
+                    maxLeftBorderSum = leftBorderSum;
+                }
+            }
+
+            int maxRightBorderSum = 0, rightBorderSum = 0;
+            for (int i = center + 1; i <= right; i++) {
+                rightBorderSum += array[i];
+                if (rightBorderSum > maxRightBorderSum) {
+                    maxRightBorderSum = rightBorderSum;
+                }
+            }
+
+            return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
+        }
+
+        private int max3(int a, int b, int c) {
+            return Math.max(Math.max(a, b), c);
+        }
+    }
+
+    /*
+    * Linear-time maximum contiguous subsequence sum algorithm.
+    * */
+    private static class LinearTimeAlgorithm implements MaximumContiguousSubSequenceSum {
+
+        @Override
+        public int max(int[] array) {
+            int maxSum = 0, thisSum = 0;
+
+            for (int i : array) {
+                thisSum += i;
+
+                if (thisSum > maxSum) {
+                    maxSum = thisSum;
+                } else if (thisSum < 0) {
+                    thisSum = 0;
+                }
+            }
+
+            return maxSum;
+        }
     }
 }
+
--- src/test/java/tw/ymeng/algorithm/proposition/MaxSubSumTest.java
+++ src/test/java/tw/ymeng/algorithm/proposition/MaxSubSumTest.java
@@ -1,31 +1,31 @@
 package tw.ymeng.algorithm.proposition;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static tw.ymeng.algorithm.proposition.MaxSubSum.maxSubSum;
+import static tw.ymeng.algorithm.proposition.MaxSubSum.DivideAndConquerAlgorithm;
+import static tw.ymeng.algorithm.proposition.MaxSubSum.LinearTimeAlgorithm;
 
 public class MaxSubSumTest {
 
-    @Test
-    public void should_get_max_sub_sum_that_is_from_left_part() {
-        int[] array = {4, 0, 5, 0, 0, -2, 0, 0};
+    private int[] array;
+    private int maxSum;
 
-        assertThat(maxSubSum(array), is(9));
+    @Before
+    public void setUp() {
+        array = new int[]{4, -3, 5, -2, -1, 2, 6, -2};
+        maxSum = 11;
     }
 
     @Test
-    public void should_get_max_sub_sum_that_is_from_right_part() {
-        int[] array = {0, 0, -2, 0, 0, 5, 0, 4};
-
-        assertThat(maxSubSum(array), is(9));
+    public void should_get_max_sub_sum_with_divide_and_conquer_algorithm() {
+        assertThat(DivideAndConquerAlgorithm.maxSubSum(array), is(maxSum));
     }
 
     @Test
-    public void shoudl_get_max_sub_sum_that_is_from_center_region() {
-        int[] array = {4, -3, 5, -2, -1, 2, 6, -2};
-
-        assertThat(maxSubSum(array), is(11));
+    public void should_get_max_sub_sum_with_linear_time_algorithm() {
+        assertThat(LinearTimeAlgorithm.maxSubSum(array), is(maxSum));
     }
 }
