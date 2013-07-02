2eb3bc2a909acd84954bd6963836b9b8dafb74ba
Meng Yu <ymeng@thoughtworks.com>
2013-07-02 21:45:28 +0800

    [ymeng] chinese-number: convert normal decimal number to chinese word

--- src/main/java/tw/ymeng/algorithm/proposition/ArabicNumber.java
+++ src/main/java/tw/ymeng/algorithm/proposition/ArabicNumber.java
@@ -1,8 +1,11 @@
 package tw.ymeng.algorithm.proposition;
 
+import java.util.Stack;
+
 public class ArabicNumber {
 
     private static final String[] chineseDigits = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+    private static final String[] carryList = {"", "拾", "伯", "仟"};
 
     private int number;
 
@@ -11,6 +14,26 @@ public class ArabicNumber {
     }
 
     public String toChineseWord() {
-        return chineseDigits[number];
+        int decimalNumber = number;
+
+        Stack<Integer> numberElements = new Stack<Integer>();
+        while (decimalNumber != 0) {
+            numberElements.push(decimalNumber % 10);
+
+            decimalNumber /= 10;
+        }
+
+        return buildChineseWord(numberElements);
+    }
+
+    private String buildChineseWord(Stack<Integer> numberElements) {
+        StringBuilder word = new StringBuilder();
+
+        while (!numberElements.empty()) {
+            word.append(chineseDigits[numberElements.pop()]);
+            word.append(carryList[numberElements.size()]);
+        }
+
+        return word.toString();
     }
 }
--- src/test/java/tw/ymeng/algorithm/proposition/ArabicNumberTest.java
+++ src/test/java/tw/ymeng/algorithm/proposition/ArabicNumberTest.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.assertThat;
 public class ArabicNumberTest {
 
     @Test
-    public void should_convert_single_digit_to_chinese_character() {
+    public void should_convert_single_digit_to_chinese_word() {
         ArabicNumber number = new ArabicNumber(2);
 
         assertThat(number.toChineseWord(), is("贰"));
+    }
+
+    @Test
+    public void should_convert_decimal_number_to_chinese_word() {
+        ArabicNumber number = new ArabicNumber(123);
+
+        assertThat(number.toChineseWord(), is("壹伯贰拾叁"));
     }
 }
