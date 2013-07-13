c93e7113adb38ece9f5e45c8f5b1c06f5821be05
Meng Yu <ymeng@thoughtworks.com>
2013-07-13 14:44:52 +0800

    [ymeng] calculation: add tests for simple subtraction, multiplication & division

--- src/test/java/tw/ymeng/algorithm/interview/calculator/TreeCalculatorTest.java
+++ src/test/java/tw/ymeng/algorithm/interview/calculator/TreeCalculatorTest.java
@@ -13,4 +13,25 @@ public class TreeCalculatorTest {
 
         assertThat(calculator.calculate(), is(3));
     }
+
+    @Test
+    public void should_calculate_simple_subtraction() {
+        TreeCalculator calculator = new TreeCalculator("2 - 1");
+
+        assertThat(calculator.calculate(), is(1));
+    }
+
+    @Test
+    public void should_calculate_simple_multiplication() {
+        TreeCalculator calculator = new TreeCalculator("1 * 2");
+
+        assertThat(calculator.calculate(), is(2));
+    }
+
+    @Test
+    public void should_calculate_simple_division() {
+        TreeCalculator calculator = new TreeCalculator("4 / 2");
+
+        assertThat(calculator.calculate(), is(2));
+    }
 }
