caed666db25a9a4a7458d1d7f3bcb3d0bc57f193
Meng Yu <ymeng@thoughtworks.com>
2013-07-13 11:58:36 +0800

    [ymeng] calculator: 1 + 2 * 3

--- src/test/java/tw/ymeng/algorithm/proposition/CalculatorTest.java
+++ src/test/java/tw/ymeng/algorithm/proposition/CalculatorTest.java
@@ -41,4 +41,11 @@ public class CalculatorTest {
 
         assertThat(calculator.calculate(), is(0.0));
     }
+
+    @Test
+    public void should_calculate_composition_of_addition_and_multiplication() {
+        Calculator calculator = new Calculator(new char[]{'1', '+', '2', '*', '3'});
+
+        assertThat(calculator.calculate(), is(7.0));
+    }
 }
