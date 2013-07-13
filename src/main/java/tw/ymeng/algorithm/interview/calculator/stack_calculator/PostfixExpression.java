98e672c6f0540149627e4c9deadde33ba3b4cac4
Meng Yu <ymeng@thoughtworks.com>
2013-07-13 21:06:47 +0800

    [ymeng] stack-calculator: transform simple infix expression

---
+++ src/main/java/tw/ymeng/algorithm/interview/calculator/stack_calculator/PostfixExpression.java
@@ -0,0 +1,48 @@
+package tw.ymeng.algorithm.interview.calculator.stack_calculator;
+
+import tw.ymeng.algorithm.interview.calculator.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class PostfixExpression {
+
+    private final char[] tokens;
+
+    public PostfixExpression(char[] tokens) {
+        this.tokens = tokens;
+    }
+
+    public char[] transform() {
+        Stack<Character> stack = new Stack<Character>();
+        List<Character> postfixTokens = new ArrayList<Character>();
+
+        for (char c : tokens) {
+            Token token = new Token(c);
+            if (token.isDigit()) {
+                postfixTokens.add(c);
+                continue;
+            }
+
+            if (token.isOperator()) {
+                stack.push(c);
+            }
+        }
+
+        while (!stack.empty()) {
+            postfixTokens.add(stack.pop());
+        }
+
+        return convertToCharArray(postfixTokens);
+    }
+
+    private char[] convertToCharArray(List<Character> list) {
+        char[] array = new char[list.size()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = list.get(i);
+        }
+
+        return array;
+    }
+}
---
+++ src/test/java/tw/ymeng/algorithm/interview/calculator/stack_calculator/PostfixExpressionTest.java
@@ -0,0 +1,21 @@
+package tw.ymeng.algorithm.interview.calculator.stack_calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PostfixExpressionTest {
+
+    @Test
+    public void should_transform_infix_expression_that_only_contains_two_operands_to_postfix() {
+        PostfixExpression postfixExpression = buildPostfix('1', '+', '2');
+
+        assertThat(postfixExpression.transform(), is(new char[]{'1', '2', '+'}));
+    }
+
+    private PostfixExpression buildPostfix(char... expression) {
+        return new PostfixExpression(expression);
+    }
+
+}
