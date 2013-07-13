1d2b37b466f990e66bdf4c81efebf988f0391609
Meng Yu <ymeng@thoughtworks.com>
2013-07-13 23:39:20 +0800

    [ymeng] stack-calculator: implement a calculator for postfix expression

---
+++ src/main/java/tw/ymeng/algorithm/interview/calculator/stack_calculator/PostfixExpressionCalculator.java
@@ -0,0 +1,50 @@
+package tw.ymeng.algorithm.interview.calculator.stack_calculator;
+
+import tw.ymeng.algorithm.interview.calculator.Token;
+
+import java.util.Stack;
+
+class PostfixExpressionCalculator {
+
+    private final char[] postfixTokens;
+
+    public PostfixExpressionCalculator(char[] postfixTokens) {
+        this.postfixTokens = postfixTokens;
+    }
+
+    public int calculate() {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (char c : postfixTokens) {
+            Token token = new Token(c);
+            if (token.isDigit()) {
+                stack.push(toInt(c));
+                continue;
+            }
+
+            int op1 = stack.pop();
+            int op2 = stack.pop();
+            stack.push(calculate(c, op1, op2));
+        }
+
+        return stack.pop();
+    }
+
+    private int calculate(char operator, int a, int b) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        throw new IllegalArgumentException("Illegal operator: " + operator);
+    }
+
+    private static Integer toInt(char c) {
+        return (int)c - 48;
+    }
+}
---
+++ src/test/java/tw/ymeng/algorithm/interview/calculator/stack_calculator/PostfixExpressionCalculatorTest.java
@@ -0,0 +1,25 @@
+package tw.ymeng.algorithm.interview.calculator.stack_calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PostfixExpressionCalculatorTest {
+
+    @Test
+    public void should_calculate_simple_postfix_expression() {
+        PostfixExpressionCalculator calculator = new PostfixExpressionCalculator(new char[]{'1', '2', '+'});
+
+        assertThat(calculator.calculate(), is(3));
+    }
+
+    @Test
+    public void should_calculate_complicated_postfix_expression() {
+        PostfixExpressionCalculator calculator = new PostfixExpressionCalculator(
+                new char[]{'1', '2', '3', '*', '+', '4', '5', '*', '6', '+', '7', '*', '+'}
+        );
+
+        assertThat(calculator.calculate(), is(189));
+    }
+}
